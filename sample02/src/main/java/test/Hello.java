4ebee87e20ad2904ffb5a750e4cc87603b79c57b
Jinyoung <jinyoung.eom@gmail.com>
2016-09-20 11:18:27 +0900

    .

--- sample02/src/main/java/test/Hello.java
+++ sample02/src/main/java/test/Hello.java
@@ -3,19 +3,26 @@ package test;
 public class Hello {
 
 	public static void main(String[] args) {
+
+		System.out.println("권태임 수정");
+
 		int age = 10;
 		if (age < 19) {
 		  System.out.println("미성년");
-		} else if (age >= 19 && age <= 39) {
-		  System.out.println("청년");
 		} else {
 		  System.out.println("성년");
 		}
 		
 		System.out.println("test..ok");
 		
+
+
     int b = 2, a = 30;
 		System.out.println("Hello~~~~~~~~~~~~~~~~~><");
+
+
+
+		
 	}
 
 }
